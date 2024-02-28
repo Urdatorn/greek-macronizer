2a17d4518ac07c24130827328e3fa37cad55f294
Urdatorn <urdatorn@hotmail.com>
2024-02-29 00:43:11 +0700

    rm some tracking

--- various/test_convert.py
+++
@@ -1,5 +0,0 @@
-from to_beta_code import translate_to_beta_code
-
-# Convert to BETA CODE
-print("Converting to BETA CODE and generating tokens_beta.txt")
-translate_to_beta_code('tokens/tokens_no_punct.txt', 'tokens/tokens_beta.txt')
